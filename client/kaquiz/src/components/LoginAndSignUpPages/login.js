3027f5455be2f74f04146f68beb4c29b053bf121
Kochela <josephngalu96@gmail.com>
2025-01-13 10:48:17 +0000

    login and signup pages updated with new css and functionality added

--- client/kaquiz/src/components/LoginAndSignUpPages/login.js
+++ client/kaquiz/src/components/LoginAndSignUpPages/login.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../../store/slices/authSlice';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { Button, Card, InputGroup } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import '../../App.css';
 
 
-function Login() {
+function DesktopView (){
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
 
 
   const formSchema = Yup.object().shape({
     userName: Yup.string()
-      .required('User name is required')
-      .min(3, 'User name must be at least 3 characters'),
+      .required('*User name is required')
+      .min(3, '*User name must be at least 3 characters'),
     password: Yup.string()
-      .min(8, 'Password is too short - should be 8 chars minimum')
-      .required('Password is required'),
+      .min(8, '*Password is too short - should be 8 chars minimum')
+      .required('*Password is required'),
   });
 
   const initialValues = {
@@ -58,38 +59,40 @@ function Login() {
             type="userName"
             name="userName"
             placeholder="Enter Your User Name"
-            className='form-control form-control-lg'
-            style={{fontSize: "14px"}}
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "14px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
             
           />
           {errors.userName && touched.userName && (
-            <ErrorMessage name="userName" component="div" />
+            <ErrorMessage  style={{fontSize: "14px"}} className='text-danger' name="userName" component="div" />
           )}
 
           <div className="mb-3 position-relative">
           <p style={{fontSize: "14px"}} className='text-left mt-3 mb-0'>Password</p>
-          <div className='d-flex flex-row' style={{position: 'relative'}}>
+          <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
             <Field
               type={showPassword ? 'text' : 'password'}
               name="password"
               placeholder="Enter Your Password"
-              className='form-control  form-control-lg text-secondary'
-              style={{fontSize: "14px", paddingRight: '35px'}}
+              className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+              style={{fontSize: "14px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
             />
             
             <button 
               type="button" 
               onClick={() => setShowPassword(!showPassword)}
-              className="btn btn-link p-0 mr-2 position-absolute top-50 translate-middle-y right-0"
+              className="btn btn-link mr-20"
               style={{color: '#000000'}}
             >
               {showPassword ? <FaEye /> : <FaEyeSlash />}
             </button>
+           
              
           </div>
           
+          
           {errors.password && touched.password && (
-            <ErrorMessage name="password" component="div" />
+            <ErrorMessage className='text-danger'  style={{fontSize: "14px"}} name="password" component="div" />
           )}
           </div>
           <div className='d-flex flex-row justify-content-between'>
@@ -99,14 +102,14 @@ function Login() {
                     name="rememberMe"
                     checked={touched.rememberMe ? touched.rememberMe : false}
                     onChange={(e) => setFieldValue('rememberMe', e.target.checked)}
-                    className="form-check-input bg-dark border-dark text-secondary"
+                    className="form-check-input  bg-dark shadow-none border-dark text-secondary"
                     style={{fontSize: "14px"}}
                   />
                   <label style={{fontSize: "14px"}} className="form-check-label" htmlFor="rememberMe">
                     Remember Me
                   </label>
                 </div>
-                <Link href="/forgot-password" style={{fontSize: "14px"}} className="text-right m-auto me-0 text-muted text-decoration-none">Forgot Password?</Link>
+                <Link href="/forgot-password" style={{fontSize: "14px", fontColor: "#ABABAB"}} className="text-right m-auto me-0 text-muted text-decoration-none">Forgot Password?</Link>
           </div>
           <Button style={{fontSize: "14px"}} className='form-control bg-dark border-0 mt-3' type="submit">Log In</Button>
         </Form>
@@ -124,5 +127,152 @@ function Login() {
     </Card>
   );
 }
+
+function MobileView (){
+  const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+
+
+  const formSchema = Yup.object().shape({
+    userName: Yup.string()
+      .required('*User name is required')
+      .min(3, '*User name must be at least 3 characters'),
+    password: Yup.string()
+      .min(8, '*Password is too short - should be 8 chars minimum')
+      .required('*Password is required'),
+  });
+
+  const initialValues = {
+    userName: '',
+    password: '',
+    rememberMe: false,
+  };
+
+  const handleSubmit = async (values) => {
+    try {
+      // Dispatch the login action with the form values
+      await dispatch(login(values));
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  return (
+    <Card style={{height: "100vh"}} className='d-flex flex-column border-0 justify-content-around'> 
+    <h1 className='text-left ms-5 fs-50'>KaQuiz</h1>
+    <Card style={{height: "100%"}} className='d-flex flex-row p-2 border-0 justify-content-center mt-20 w-100'>
+      <Card className='w-100 min-w-50 mr-3 p-3'>
+        <Card.Title style={{fontFamily: "'Poppins', sans-serif"}} className='text-left mb-2 fs-5 fw-normal'>Welcome !</Card.Title>
+        <Card.Text className='text-left mt-2 mb-0 fw-bold fs-4'>Sign in to</Card.Text>
+        <Card.Text className='text-left mb-2' style={{fontSize: "14px"}}>KaQuiz is simply </Card.Text>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={formSchema}
+      onSubmit={handleSubmit}
+      className='d-flex flex-column p-10'
+    >
+      {({ errors, touched,  setFieldValue, isSubmitting, values, formProps }) => (
+        <Form className='d-flex flex-column' onSubmit={handleSubmit}>
+          <p style={{fontSize: "14px"}} className='text-left mt-2 mb-0'>User name</p>
+          <Field
+            type="userName"
+            name="userName"
+            placeholder="Enter Your User Name"
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "14px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            
+          />
+          {errors.userName && touched.userName && (
+            <ErrorMessage  style={{fontSize: "14px"}} className='text-danger' name="userName" component="div" />
+          )}
+
+          <div className="mb-3 position-relative">
+          <p style={{fontSize: "14px"}} className='text-left mt-3 mb-0'>Password</p>
+          <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
+            <Field
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder="Enter Your Password"
+              className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+              style={{fontSize: "14px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            />
+            
+            <button 
+              type="button" 
+              onClick={() => setShowPassword(!showPassword)}
+              className="btn btn-link mr-20"
+              style={{color: '#000000'}}
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </button>
+           
+             
+          </div>
+          
+          
+          {errors.password && touched.password && (
+            <ErrorMessage className='text-danger'  style={{fontSize: "14px"}} name="password" component="div" />
+          )}
+          </div>
+          <div className='d-flex flex-row justify-content-between'>
+          <div className="form-check mt-2 mb-2">
+                  <Field
+                    type="checkbox"
+                    name="rememberMe"
+                    checked={touched.rememberMe ? touched.rememberMe : false}
+                    onChange={(e) => setFieldValue('rememberMe', e.target.checked)}
+                    className="form-check-input  bg-dark shadow-none border-dark text-secondary"
+                    style={{fontSize: "14px"}}
+                  />
+                  <label style={{fontSize: "14px"}} className="form-check-label" htmlFor="rememberMe">
+                    Remember Me
+                  </label>
+                </div>
+                <Link href="/forgot-password" style={{fontSize: "14px", fontColor: "#ABABAB"}} className="text-right m-auto me-0 text-muted text-decoration-none">Forgot Password?</Link>
+          </div>
+          <Button style={{fontSize: "14px"}} className='form-control bg-dark border-0 mt-2' type="submit">Log In</Button>
+        </Form>
+      )}
+    </Formik>
+    <div className='d-flex justify-content-center mt-3'>
+        <p style={{fontSize: "14px", color: "#7D7D7D"}}>Don't have an account?</p>
+        <Link href="/register" style={{fontSize: "14px", marginLeft: '10px', color: '#000000'}} className="text-decoration-none">Register</Link>
+      </div>
+    </Card>
+    
+    </Card>
+    </Card>
+  );
+}
+
+
+
+function Login() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const isSmallScreenThreshold = 768;
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      setIsSmallScreen(width <= isSmallScreenThreshold);
+    };
+  
+    // Check initial screen size
+    checkScreenSize();
+  
+    // Listen for window resize events
+    window.addEventListener('resize', checkScreenSize);
+  
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
+  return isSmallScreen ? <MobileView /> : <DesktopView />;
+  
+}
+
+
 
 export default Login;
--- client/kaquiz/src/components/LoginAndSignUpPages/signUp.js
+++ client/kaquiz/src/components/LoginAndSignUpPages/signUp.js
@@ -1,88 +1,340 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { signup } from '../../store/slices/authSlice';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import '../../App.css';
 
-const Signup = () => {
+function DesktopView(){
   const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
 
   const formSchema = Yup.object().shape({
-    firstName: Yup.string()
-      .max(15, 'Must be 15 characters or less')
-      .required('First name is required'),
-    lastName: Yup.string()
-      .max(20, 'Must be 20 characters or less')
-      .required('Last name is required'),
     email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required'),
+      .email('*Invalid email address')
+      .required('*Email is required'),
     password: Yup.string()
-      .min(8, 'Password is too short - should be 8 chars minimum')
-      .required('Password is required'),
+      .min(8, '*Password is too short - should be 8 chars minimum')
+      .required('*Password is required'),
+      confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], '*Passwords must match')
+      .required('*Confirm password is required'),
   });
 
-  const signupForm = {
-    firstName: '',
-    lastName: '',
+  const initialValues = {
+    userName: '',
     email: '',
     password: '',
+    confirmPassword: '',
   };
 
-  const handleSubmit = async (values, { setSubmitting }) => {
+  const handleSubmit = async (values) => {
     try {
       // Dispatch the signup action with the form values
       await dispatch(signup(values));
-      setSubmitting(false);
     } catch (error) {
       console.error('Signup failed:', error);
-      setSubmitting(false);
     }
   };
 
   return (
+    <Card style={{height: "100vh"}} className='d-flex flex-column border-0 justify-content-around'> 
+    <h1 className='text-left ms-5 fs-50'>KaQuiz</h1>
+    <Card style={{height: "85%"}} className='d-flex p-3 flex-row border-0 justify-content-center mt-20 w-100'>
+      <Card className='w-25 ml-3 mr-3 p-3'>
+        <Card.Title style={{fontFamily: "'Poppins', sans-serif"}} className='text-left mb-2 fs-5 fw-normal'>Welcome!</Card.Title>
+        <Card.Text className='text-left mt-2 mb-0 fw-bold fs-4'>Sign up to</Card.Text>
+        <Card.Text className='text-left mb-3' style={{fontSize: "12px"}}>KaQuiz is simply </Card.Text>
+
     <Formik
-      initialValues={signupForm}
+      initialValues={initialValues}
       validationSchema={formSchema}
       onSubmit={handleSubmit}
+      className='d-flex flex-column p-10'
     >
-      {({ isSubmitting }) => (
-        <Form onSubmit={!isSubmitting ? undefined : handleSubmit}>
+      {({ errors, touched, setFieldValue, isSubmitting, values, formProps }) => (
+        <Form className='d-flex flex-column' onSubmit={handleSubmit}>
+          <p style={{fontSize: "12px"}} className='text-left mt-3 mb-0'>Email</p>
           <Field
             type="text"
-            name="firstName"
-            placeholder="First Name"
-          />
-          <ErrorMessage name="firstName" component="div" />
-
-          <Field
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-          />
-          <ErrorMessage name="lastName" component="div" />
-
-          <Field
-            type="email"
             name="email"
-            placeholder="Email"
+            placeholder="Enter Your Email"
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "12px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
           />
-          <ErrorMessage name="email" component="div" />
+          {errors.email && touched.email && (
+            <ErrorMessage  style={{fontSize: "12px"}} className='text-danger' name="email" component="div" />
+          )}
+          
 
+          <p style={{fontSize: "12px"}} className='text-left mt-3 mb-0'>User name</p>
           <Field
-            type="password"
-            name="password"
-            placeholder="Password"
+            type="userName"
+            name="userName"
+            placeholder="Enter Your User Name"
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "12px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            
           />
-          <ErrorMessage name="password" component="div" />
+          {errors.userName && touched.userName && (
+            <ErrorMessage  style={{fontSize: "14px"}} className='text-danger' name="userName" component="div" />
+          )}
 
-          <button type="submit" disabled={isSubmitting}>
-            Sign Up
-          </button>
+          <div className="mb-3 position-relative">
+          <p style={{fontSize: "12px"}} className='text-left mt-3 mb-0'>Password</p>
+          <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
+            <Field
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder="Enter Your Password"
+              className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+              style={{fontSize: "12px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            />
+            
+            <button 
+              type="button" 
+              onClick={() => setShowPassword(!showPassword)}
+              className="btn btn-link mr-20"
+              style={{color: '#000000'}}
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </button>
+          
+            
+          </div>
+          
+          
+          {errors.password && touched.password && (
+            <ErrorMessage className='text-danger'  style={{fontSize: "14px"}} name="password" component="div" />
+          )}
+          </div>
+
+          <div className="mb-3 position-relative">
+            <p style={{fontSize: "12px"}} className='text-left mt-0 mb-0'>Confirm Password</p>
+            <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
+              <Field
+                type="password"
+                name="confirmPassword"
+                placeholder="Enter Your Confirm Password"
+                className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+                style={{fontSize: "12px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+              />
+              
+              <button 
+                type="button" 
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="btn btn-link mr-20"
+                style={{color: '#000000'}}
+              >
+                {showConfirmPassword? <FaEye /> : <FaEyeSlash />}
+              </button>
+             
+            
+            
+          </div>
+          
+          {errors.confirmPassword && touched.confirmPassword && (
+            <ErrorMessage className='text-danger' style={{fontSize: "14px"}} name="confirmPassword" component="div" />
+          )}
+          </div>
+          
+          <Button style={{fontSize: "12px"}} className='form-control bg-dark border-0 mt-1' type="submit">Register</Button>
         </Form>
       )}
     </Formik>
+    
+    <div className='d-flex justify-content-center mt-3'>
+        <p style={{fontSize: "12px", color: "#7D7D7D"}}>Already have an account?</p>
+        <Link href="/login" style={{fontSize: "12px", marginLeft: '10px', color: '#000000'}} className="text-decoration-none">Register</Link>
+      </div>
+    </Card>
+    <Card className='border-0 ml-3 p-3' style={{maxWidth: '110vh'}} >
+      <img className="h-100" src="/assets/small-team-discussing-ideas-2194220-0.png" alt='team' />
+    </Card>
+    </Card>
+    </Card>
   );
-};
+}
+
+function MobileView(){
+  const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+
+  const formSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('*Invalid email address')
+      .required('*Email is required'),
+    password: Yup.string()
+      .min(8, '*Password is too short - should be 8 chars minimum')
+      .required('*Password is required'),
+      confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], '*Passwords must match')
+      .required('*Confirm password is required'),
+  });
+
+  const initialValues = {
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  const handleSubmit = async (values) => {
+    try {
+      // Dispatch the signup action with the form values
+      await dispatch(signup(values));
+    } catch (error) {
+      console.error('Signup failed:', error);
+    }
+  };
+
+  return (
+    <Card style={{height: "100vh"}} className='d-flex flex-column border-0 justify-content-around'> 
+    <h1 className='text-left ms-5 fs-50'>KaQuiz</h1>
+    <Card style={{height: "100%"}} className='d-flex flex-row p-2 border-0 justify-content-center mt-20 w-100'>
+      <Card className='w-100 min-w-30  mr-3 p-3'>
+        <Card.Title style={{fontFamily: "'Poppins', sans-serif"}} className='text-left mb-2 fs-5 fw-normal'>Welcome!</Card.Title>
+        <Card.Text className='text-left mt-2 mb-0 fw-bold fs-4'>Sign up to</Card.Text>
+        <Card.Text className='text-left mb-2' style={{fontSize: "12px"}}>KaQuiz is simply </Card.Text>
+
+    <Formik
+      initialValues={initialValues}
+      validationSchema={formSchema}
+      onSubmit={handleSubmit}
+      className='d-flex flex-column p-10'
+    >
+      {({ errors, touched, setFieldValue, isSubmitting, values, formProps }) => (
+        <Form className='d-flex flex-column' onSubmit={handleSubmit}>
+          <p style={{fontSize: "12px"}} className='text-left mt-2 mb-0'>Email</p>
+          <Field
+            type="text"
+            name="email"
+            placeholder="Enter Your Email"
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "12px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+          />
+          {errors.email && touched.email && (
+            <ErrorMessage  style={{fontSize: "12px"}} className='text-danger' name="email" component="div" />
+          )}
+          
+
+          <p style={{fontSize: "12px"}} className='text-left mt-2 mb-0'>User name</p>
+          <Field
+            type="userName"
+            name="userName"
+            placeholder="Enter Your User Name"
+            className='form-control form-control-lg shadow-none border rounded-3'
+            style={{fontSize: "12px", borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            
+          />
+          {errors.userName && touched.userName && (
+            <ErrorMessage  style={{fontSize: "14px"}} className='text-danger' name="userName" component="div" />
+          )}
+
+          <div className="mb-3 position-relative">
+          <p style={{fontSize: "12px"}} className='text-left mt-2 mb-0'>Password</p>
+          <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
+            <Field
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder="Enter Your Password"
+              className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+              style={{fontSize: "12px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+            />
+            
+            <button 
+              type="button" 
+              onClick={() => setShowPassword(!showPassword)}
+              className="btn btn-link mr-20"
+              style={{color: '#000000'}}
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </button>
+          
+            
+          </div>
+          
+          
+          {errors.password && touched.password && (
+            <ErrorMessage className='text-danger'  style={{fontSize: "14px"}} name="password" component="div" />
+          )}
+          </div>
+
+          <div className="mb-3 position-relative">
+            <p style={{fontSize: "12px"}} className='text-left mt-0 mb-0'>Confirm Password</p>
+            <div className='d-flex flex-row border p-0 rounded-3 form-control  form-control-lg'>
+              <Field
+                type="password"
+                name="confirmPassword"
+                placeholder="Enter Your Confirm Password"
+                className='form-control  form-control-lg text-secondary border-0 shadow-none' 
+                style={{fontSize: "12px", paddingRight: '35px', borderColor: '#ABABAB', fontColor: '#ABABAB'}}
+              />
+              
+              <button 
+                type="button" 
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="btn btn-link mr-20"
+                style={{color: '#000000'}}
+              >
+                {showConfirmPassword? <FaEye /> : <FaEyeSlash />}
+              </button>
+             
+            
+            
+          </div>
+          
+          {errors.confirmPassword && touched.confirmPassword && (
+            <ErrorMessage className='text-danger' style={{fontSize: "14px"}} name="confirmPassword" component="div" />
+          )}
+          </div>
+          
+          <Button style={{fontSize: "12px"}} className='form-control bg-dark border-0 mt-1' type="submit">Register</Button>
+        </Form>
+      )}
+    </Formik>
+    
+    <div className='d-flex justify-content-center mt-2'>
+        <p style={{fontSize: "12px", color: "#7D7D7D"}}>Already have an account?</p>
+        <Link href="/login" style={{fontSize: "12px", marginLeft: '10px', color: '#000000'}} className="text-decoration-none">Register</Link>
+      </div>
+    </Card>
+    </Card>
+    </Card>
+  );
+}
+
+function Signup() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const isSmallScreenThreshold = 768;
+  
+    useEffect(() => {
+      const checkScreenSize = () => {
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        setIsSmallScreen(width <= isSmallScreenThreshold);
+      };
+    
+      // Check initial screen size
+      checkScreenSize();
+    
+      // Listen for window resize events
+      window.addEventListener('resize', checkScreenSize);
+    
+      // Cleanup function to remove event listener
+      return () => {
+        window.removeEventListener('resize', checkScreenSize);
+      };
+    }, []);
+  
+    return isSmallScreen ? <MobileView /> : <DesktopView />;
+  
+}
 
 export default Signup;
